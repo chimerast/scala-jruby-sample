13b9e3e8984976b93ed0771ff4b2922046331771
Hideyuki TAKEUCHI <webmaster@chimera.st>
2011-03-21 00:51:03 +0900

    JSR223を使うのをやめた

--- project/build/ScalaJRubyProject.scala
+++ project/build/ScalaJRubyProject.scala
@@ -2,7 +2,6 @@ import sbt._
 
 class ScalaJRubyProject(info: ProjectInfo) extends DefaultProject(info) {
   val jruby = "org.jruby" % "jruby" % "1.6.0"
-  val jsr223 = "org.livetribe" % "livetribe-jsr223" % "2.0.6"
 
   override def fork = forkRun
 }
--- src/main/scala/org/karatachi/scala/Bootstrap.scala
+++ src/main/scala/org/karatachi/scala/Bootstrap.scala
@@ -1,18 +1,33 @@
 package org.karatachi.scala
 
 import java.io._
-import javax.script._
+
+import scala.collection.JavaConversions._
+
+import org.jruby.javasupport._
 
 object Bootstrap {
+  val script = """
+require 'java'
+class Test
+  include java.lang.Runnable
+  def run
+    puts "Hello World"
+  end
+end
+
+Test.new
+  """
+
   def main(args: Array[String]) {
-    println(JRubyEngine.eval("1"))
+    val scene = JRuby.eval(script).asInstanceOf[Runnable]
+    scene.run
   }
 }
 
-object JRubyEngine {
-  val manager = new ScriptEngineManager
-  val engine = manager.getEngineByName("jruby")
+object JRuby {
+  val ruby = JavaEmbedUtils.initialize(List())
+  val evaluator = JavaEmbedUtils.newRuntimeAdapter
 
-  def eval(script: String) = engine.eval(script)
-  def eval(reader: Reader) = engine.eval(reader)
+  def eval(script: String) = evaluator.eval(ruby, script)
 }
